3e055b7d55798771c05c2e32cbcf13b4fc1d751e
Saulo Freitas <69553799+SauloFreitas01@users.noreply.github.com>
2023-05-22 00:05:44 -0300

    ADD project

---
+++ src/main/java/com/mycompany/blioteca/Biblioteca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.blioteca;
+
+/**
+ *
+ * @author saulo
+ */
+public class Biblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
