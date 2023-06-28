675a8586308404065a0cb5d92db450d5d959245a
Marcelo Vitor <marcelovitor890@gmail.com>
2023-06-28 18:16:23 -0300

    Criação arquivos ".bin"

--- src/Frames/SignUpPage.java
+++ src/Frames/SignUpPage.java
@@ -284,13 +284,13 @@ public class SignUpPage extends javax.swing.JFrame {
      */
     public static void main(String args[]) throws IOException, IOException {
         //Criação dos arquivos ".bin"
-        ArrayList<Member> listaUsuarios = new ArrayList<>();
+        /*ArrayList<Member> listaUsuarios = new ArrayList<>();
         ArrayList<Book> listaLivros = new ArrayList<>();
         ArrayList<Book> listaEmprestimos = new ArrayList<>();
         
         escreverArquivo(listaUsuarios, "usuarios.bin");
         escreverArquivo(listaLivros, "livros.bin");
-        escreverArquivo(listaEmprestimos, "emprestimos.bin");
+        escreverArquivo(listaEmprestimos, "emprestimos.bin");*/
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
